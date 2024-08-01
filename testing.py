ce5baa38564138f0afaf2bd820d77aa79c087428
DefineBox <reblp2005@gmail.com>
2024-07-31 21:29:31 -0500

    Added more print lines along with a for loop in testing.py

--- testing.py
+++ testing.py
@@ -1,2 +1,12 @@
 print("Testing....1..2..3..")
 print("Hello World!")
+print()
+print("Now it's time for something new...")
+
+i = 10
+for i in range(10):
+    print(i)
+    i -= 1
+
+print("Time's up!")
+
