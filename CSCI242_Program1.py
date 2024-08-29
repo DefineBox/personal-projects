2b69782e9dbd6fa32d225d8121c3c47a06b5a42f
DefineBox <reblp2005@gmail.com>
2024-08-29 10:18:54 -0500

    Added the file CSCI242_Program1.py to the staging area. Now, I will commit the changes.

---
+++ CSCI242_Program1.py
@@ -0,0 +1,23 @@
+#pop - removes the top item
+#push - adds to the top of the list
+#top - removes the top item from the stack
+#peak - returns the top item from the stack
+
+class Liststack:
+
+    def __init__(self, maxSize = 10):
+            self.__stack = [None] * maxSize
+            self.__numOfItems = 0
+
+    def push(self, itemToPush):
+        pass
+    
+    def peak(self):
+        pass
+    
+    def pop(self):
+        pass
+            
+    def top(self):
+        pass
+            
