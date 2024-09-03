a5cde1298c12df88f0a15106cf13025efc897cf1
DefineBox <reblp2005@gmail.com>
2024-09-03 10:06:31 -0500

    feat: Add CSCI242_Program2.py with main function and Liststack import

---
+++ CSCI242_Program2.py
@@ -0,0 +1,15 @@
+from CSCI242_Program1 import *
+import Liststack
+
+def main():
+    stack1 = Liststack(5)
+    stack1.push("a")
+    Liststack.push(stack1, "a")
+    stack1.push("a")
+    stack1.push("d") 
+
+main()
+
+
+
+
